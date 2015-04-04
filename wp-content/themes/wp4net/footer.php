3535ef859705232ba9db4d70da485cad5f215c68
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2015-04-04 08:42:11 -0300

    resolve #6 - cria footer em html

--- wp-content/themes/wp4net/footer.php
+++ wp-content/themes/wp4net/footer.php
@@ -1,31 +1,48 @@
-	<section id="footer" class="container">
-		<div class="col-sm-4">
-			<h1>Produtos</h1>
-			<ul>
-				<li>Hospedagem</li>
-				<li>Gerenciamento de projeto</li>
-				<li>Arquitetura da informação</li>
-				<li>SEO</li>
-				<li>Timeline BackUP</li>
-			</ul>
-		</div>
-		<div class="col-sm-4">
-			<h1>Suporte</h1>
-			<ul>
-				<li>Chat</li>
-				<li>Ticket</li>
-				<li>Base de conhecimento</li>
-				<li>Forum</li>
-			</ul>
-		</div>
-		<div class="col-sm-4">
-			<h1>Transparência</h1>
-			<ul>
-				<li>Missão e Visão</li>
-				<li>Valores</li>
-				<li>Relatório Financeiro</li>
-				<li>Termos de uso</li>
-			</ul>
+	<section id="footer">
+		<div class="container">
+			<div class="row footer-nav">
+				<div class="col-sm-4">
+					<h1>Produtos</h1>
+					<ul>
+						<li>Hospedagem</li>
+						<li>Gerenciamento de projeto</li>
+						<li>Arquitetura da informação</li>
+						<li>SEO</li>
+						<li>Timeline BackUP</li>
+					</ul>
+				</div>
+				<div class="col-sm-4">
+					<h1>Suporte</h1>
+					<ul>
+						<li>Chat</li>
+						<li>Ticket</li>
+						<li>Base de conhecimento</li>
+						<li>Forum</li>
+					</ul>
+				</div>
+				<div class="col-sm-4">
+					<h1>Transparência</h1>
+					<ul>
+						<li>Missão e Visão</li>
+						<li>Valores</li>
+						<li>Relatório Financeiro</li>
+						<li>Termos de uso</li>
+					</ul>
+				</div>
+			</div>
+			<hr />
+			<div class="btn-group pull-left">
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/Wordpress-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/Mail-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/Facebook-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/Google_-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/LinkedIn-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/YouTube-128.png'; ?> "></a>
+				<a href="#" class="btn"><img src="<?php echo get_stylesheet_directory_uri().'/img/RSS-128.png'; ?> "></a>
+			</div>
+			<span class="pull-right">
+				<p>© 2015 - Rafael D´Alessandro Pires</p>
+			</span>
 		</div>
 
 
