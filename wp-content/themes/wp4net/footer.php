055ff92620ce3b2795a50007626769e284f340dc
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2015-04-04 07:45:12 -0300

    adiciona icones sociais
    adiciona icones sociais

--- wp-content/themes/wp4net/footer.php
+++ wp-content/themes/wp4net/footer.php
@@ -1,5 +1,34 @@
 	<section id="footer" class="container">
-	nada footer
+		<div class="col-sm-4">
+			<h1>Produtos</h1>
+			<ul>
+				<li>Hospedagem</li>
+				<li>Gerenciamento de projeto</li>
+				<li>Arquitetura da informação</li>
+				<li>SEO</li>
+				<li>Timeline BackUP</li>
+			</ul>
+		</div>
+		<div class="col-sm-4">
+			<h1>Suporte</h1>
+			<ul>
+				<li>Chat</li>
+				<li>Ticket</li>
+				<li>Base de conhecimento</li>
+				<li>Forum</li>
+			</ul>
+		</div>
+		<div class="col-sm-4">
+			<h1>Transparência</h1>
+			<ul>
+				<li>Missão e Visão</li>
+				<li>Valores</li>
+				<li>Relatório Financeiro</li>
+				<li>Termos de uso</li>
+			</ul>
+		</div>
+
+
 	</section>
 
 <?php wp_footer(); ?>
