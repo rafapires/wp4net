730eae5356e9cee08d000592158fb3b7ce17dc40
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2015-04-04 06:43:20 -0300

    resolve #4 ajustando menu com logo horizontal

--- wp-content/themes/wp4net/header.php
+++ wp-content/themes/wp4net/header.php
@@ -38,21 +38,20 @@
 
     <section id="header">
       <div class="container">
-        <nav class="navbar navbar-default">
+
+        <nav class="navbar navbar-inverse">
           <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#"><img src="<?php echo get_stylesheet_directory_uri().'/img/logo-h-100h.jpg'; ?>" class="img-responsive"></a>
+              <a class="navbar-brand" href="#"><img src="<?php echo get_stylesheet_directory_uri().'/img/logo-h-100h.jpg' ?>"></a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
               <ul class="nav navbar-nav">
                 <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
                 <li><a href="#">Link</a></li>
@@ -77,20 +76,12 @@
               </form>
               <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Link</a></li>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                  <ul class="dropdown-menu" role="menu">
-                    <li><a href="#">Action</a></li>
-                    <li><a href="#">Another action</a></li>
-                    <li><a href="#">Something else here</a></li>
-                    <li class="divider"></li>
-                    <li><a href="#">Separated link</a></li>
-                  </ul>
-                </li>
               </ul>
-            </div><!-- /.navbar-collapse -->
-          </div><!-- /.container-fluid -->
+            </div>
+          </div>
         </nav>
+        
+
       </div>
     </section>
     
