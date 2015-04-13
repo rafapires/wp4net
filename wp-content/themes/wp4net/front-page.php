83da9c4fa0f123ebd365a49ceeb0ede51e1d4075
RAFAEL D ALESSANDRO PIRES <rafael.pires.sp@gmail.com>
2015-04-13 05:32:19 -0300

    coloca conteudo principal na home

--- wp-content/themes/wp4net/front-page.php
+++ wp-content/themes/wp4net/front-page.php
@@ -5,7 +5,7 @@
 get_header(); ?>
 
 <?php $args_carrocel = array(
-  'order' => 'DESC',
+  'order' => 'ASC',
   'orderby' => 'meta_value_num',
   'meta_key' => 'carrocel',
   'post_type' => 'page',
@@ -30,14 +30,13 @@ $pages = $query_carrocel->get_posts();
   <div class="carousel-inner" role="listbox">
     <?php $item_active=' active';
     foreach ($pages as $page) { 
-
       ?>
       <div class="item<?php echo $item_active; ?>">
         <?php echo get_the_post_thumbnail($page->ID); ?>
         <div class="carousel-caption">
           <h1><?php echo $page->post_title; ?></h1>
           <p><?php if ($page->post_excerpt) { echo $page->post_excerpt;} else {echo substr(strip_tags($page->post_content),0,240);} ?></p>
-          <a href="#" class="btn btn-warning">Saiba mais</a>
+          <a href="#<?php echo $page->post_name; ?>" class="btn btn-warning">Saiba mais</a>
         </div>
       </div>
 
@@ -60,6 +59,43 @@ $pages = $query_carrocel->get_posts();
 
 <div class="container">
 
+<span id="filosofia-da-colaboratividade">
+    <h1>Filosofia da Colaboratividade (nossos valores)</h1>
+    <ul>
+      <li>O conhecimento de um ajuda o desenvolvimento de todos</li>
+      <li>Quanto mais colaborativo é a WP4NET mais barato fica para todos</li>
+      <li>Quanto mais colaborativo mais amplo e rápido é o FLUXO de conhecimento garantindo eficiência, agilidade e OPORTUNIDADES INOVADORAS</li>
+    </ul>
+    <a href="#" class="btn btn-block">Mais informações</a>
+</span>
+<span id="hospedagem">
+    <h1>Hospedagem só para WordPress</h1>
+    <ul>
+      <li>Configuração do servidor 100% compatível com as diretrizes do WordPress</li>
+      <li>Mais segurança e velocidade</li>
+      <li>Maior compatibilidade com plugins</li>
+      <li>Erros encontrados em um site de um associado é automáticamente compartilhado com todos os outros, aumentando a confiabilidade do site e minimizando custos</li>
+      <li>Backups Diários e atualizações constantes garantem segurança e a utilização do que há de mais novo no WordPress</li>
+    </ul>
+    <a href="#" class="btn btn-block">Mais informações</a>
+<span id="transparencia">
+    <h1>Transparência</h1>
+    <ul>
+      <li>Gestão</li>
+      <ul>
+        <li>Demonstrativo do plano de ação</li>
+        <li>Chamada de associados para tomada de decisões e prioridade de ações</li>
+        <li>Livre participação de associados em comitês de interesse</li>
+      </ul>
+      <li>Financeiro</li>
+      <ul>
+        <li>Transparência total dos custos</li>
+        <li>Participação livre de associados na análise e auditoria</li>
+        <li>Definição de preços pelo plano de ação de gestão, quanto mais colaborativo mais justo</li>
+      </ul>
+    </ul>
+    <a href="#" class="btn btn-block">Mais informações</a>
+</span>
 </div>
 
 <?php get_footer(); ?>
